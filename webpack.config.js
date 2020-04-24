19d9d28bbae3c75014fdbda2bf92d06899fcd0f4
RPEA <richard.peach@softwareag.com>
2020-04-24 18:57:49 +0100

    Adding useful external packages and whitelisting all dependencies for the ng-packagr step

--- webpack.config.js
+++ webpack.config.js
@@ -27,10 +27,14 @@ module.exports = {
             filter: null,
             debug: true,
             useExternals: {
+                "@angular/animations": "@angular/animations",
                 "@angular/common": "@angular/common",
+                "@angular/common/http": "@angular/common/http",
                 "@angular/core": "@angular/core",
                 "@angular/forms": "@angular/forms",
                 "@angular/http": "@angular/http",
+                "@angular/platform-browser": "@angular/platform-browser",
+                "@angular/platform-browser/animations": "@angular/platform-browser/animations",
                 "@angular/router": "@angular/router",
                 "@c8y/client": "@c8y/client",
                 "@c8y/ngx-components": "@c8y/ngx-components"
