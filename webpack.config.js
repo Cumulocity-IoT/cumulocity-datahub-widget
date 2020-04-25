e5558604c56cdd7676f177b799f00e88c62a047c
RPEA <richard.peach@softwareag.com>
2020-04-25 11:40:22 +0100

    Moving to production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const URLImportPlugin  = require("webpack-external-import/webpack");
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     devtool: 'source-map',
     module: {
         rules: [
@@ -23,21 +23,18 @@ module.exports = {
             fileName: "importManifest.js",
             basePath: '',
             publicPath: `/apps/${require('./cumulocity').contextPath}/`,
-            writeToFileEmit: false,
-            filter: null,
-            debug: true,
             useExternals: {
-                "@angular/animations": "@angular/animations",
-                "@angular/common": "@angular/common",
-                "@angular/common/http": "@angular/common/http",
-                "@angular/core": "@angular/core",
-                "@angular/forms": "@angular/forms",
-                "@angular/http": "@angular/http",
-                "@angular/platform-browser": "@angular/platform-browser",
-                "@angular/platform-browser/animations": "@angular/platform-browser/animations",
-                "@angular/router": "@angular/router",
-                "@c8y/client": "@c8y/client",
-                "@c8y/ngx-components": "@c8y/ngx-components"
+                "@angular/animations": "AngularAnimations",
+                "@angular/common": "AngularCommon",
+                "@angular/common/http": "AngularCommonHttp",
+                "@angular/core": "AngularCore",
+                "@angular/forms": "AngularForms",
+                "@angular/http": "AngularHttp",
+                "@angular/platform-browser": "AngularPlatformBrowser",
+                "@angular/platform-browser/animations": "AngularPlatformBrowserAnimations",
+                "@angular/router": "AngularRouter",
+                "@c8y/client": "C8yClient",
+                "@c8y/ngx-components": "C8yNgxComponents"
             },
         })
     ],
