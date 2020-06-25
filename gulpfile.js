c991436d8dab3820bace628689dd5066f3742558
RPEA <richard.peach@softwareag.com>
2020-06-25 14:40:09 +0100

    Adding npm installable Angular Library to output

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const zip = require('gulp-zip');
 const ngPackagr = require('ng-packagr');
 const fs = require('fs-extra');
 const del = require('del');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 function clean() {
     return del(['dist']);
@@ -13,7 +15,8 @@ function clean() {
 
 function compile() {
     return ngPackagr.build({project: './ng-package.json'})
-        .then(() => fs.copy('./dist/ng-packagr-output/fesm5', './dist/bundle-src'))
+        .then(() => fs.copy('./dist/widget-library/fesm5', './dist/bundle-src'))
+        .then(() => exec("npm pack ./widget-library", { cwd: './dist' }))
 }
 
 function bundle() {
@@ -41,4 +44,9 @@ function bundle() {
 exports.clean = clean;
 exports.build = compile;
 exports.bundle = bundle;
-exports.default = series(clean, compile, bundle);
+exports.default = series(clean, compile, bundle, async function success() {
+    console.log("Build Finished Successfully!");
+    console.log("Runtime Widget Output (Install in the browser): dist/widget.zip");
+    const pkgJson = require('./dist/widget-library/package.json');
+    console.log(`Widget Angular Library (Install with: "npm i <filename.tgz>"): dist/${pkgJson.name}-${pkgJson.version}.tgz`);
+});
